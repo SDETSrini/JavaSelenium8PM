54561e079c87402174de2389725aa40c8ad1ddec
Rajendra2327 <113188548+Rajendra2327@users.noreply.github.com>
2022-09-11 21:59:43 +0530

    Update ExAlert.java

--- src/test/java/com/qa/test/ExAlert.java
+++ src/test/java/com/qa/test/ExAlert.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ExAlert {
+public class ExAlert {                                              //rajendra eswararaju modified content
+	                                                                           //selenium8pm
 
 	public static void main(String[] args) {
 		
