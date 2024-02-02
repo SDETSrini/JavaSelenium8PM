629bf410baec2b5b4adc1eea2e21cb0040e9e0a2
RAJENDRA <RAJENDRA@LAPTOP-JP3K0KLU>
2024-02-02 11:29:42 +0530

    browser updated commit

--- src/test/java/com/qa/base/core.java
+++ src/test/java/com/qa/base/core.java
@@ -4,7 +4,8 @@ public class core {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ /// pleease update chrome browser 
+		//suggested by rajendra eswararaju//
 	}
 
 }
